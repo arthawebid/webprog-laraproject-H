95780bbb4b2115a99620812a42b42d6f47c47549
MhSTIKI <arthawebid@gmail.com>
2025-12-10 18:55:51 +0800

    Update app.blade.php

--- laraproj/resources/views/layouts/app.blade.php
+++ laraproj/resources/views/layouts/app.blade.php
@@ -33,6 +33,29 @@
 
     @yield('konten')
 
+    <div class="position-fixed bottom-0 end-0 p-3" style="z-ndex:9999;">
+      @if(session('success'))
+      <div id="toastNotif" class="toast show text-white bg-success" role="alert">
+        <div class="d-flex">
+          <div class="toast-body">
+            {{ session('success') }}
+          </div>  
+          <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"></button>
+        </div>
+      </div>
+      @endif
+    </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script>
+      document.addEventListener('DOMContentLoaded', ()=>{
+        const toastEl = document.getElementById('toastNotif')
+        if(toastEl){
+          const toast = new bootstrap.toast(toastEl, {delay: 3000})
+          toast.show()
+        }
+      })
+      
+    </script>
   </body>
 </html>
